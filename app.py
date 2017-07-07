9856c39a93229f74dbf6633fb757493af5e2d546
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-07-07 11:43:18 +0100

    added util methods to blockchain class

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from src.block import Block
+from src.blockchain import Blockchain
 
 
 if __name__ == '__main__':
     block1 = Block('Data :)', None)
     block2 = Block('More data :)', block1.hash)
 
-    block1.print_self()
-    block2.print_self()
+    bc = Blockchain()
+
+    bc.add_block(block2)
+
+    print(bc.__contains__(block1))
--- src/blockchain.py
+++ src/blockchain.py
@@ -2,6 +2,8 @@
 This class links Block objects together to form a chain.
 """
 
+from .block import Block
+
 
 class Blockchain:
     def __init__(self):
@@ -9,7 +11,24 @@ class Blockchain:
         self.blocks = {}
 
     def add_block(self, block):
+        if isinstance(block, list):
+            for b in block:
+                self.add_block(b)
+
         if self.blocks.get(block.hash) is not None:
             raise AssertionError(
                 '{} already exists on the blockchain.'.format(block.__str__()))
         self.blocks[block.hash] = block
+
+    def __len__(self):
+        return len(self.blocks)
+
+    def __getitem__(self, item):
+        if not isinstance(item, str):
+            raise TypeError()
+        return self.blocks.get(item)
+
+    def __contains__(self, item):
+        if not isinstance(item, Block):
+            raise TypeError()
+        return bool(self.blocks.get(item.hash))
